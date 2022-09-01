d15c3a3954b76a6257fdf26f7fdd0648fd0f2f00
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2022-09-01 16:02:38 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+
+function App() {
+  return (
+    <div >
+        <PersonCard firstName="Reggie" lastName="Murphree" age={24} hairColor="Brown" />
+        <PersonCard firstName="Harold" lastName="Muppet" age={60} hairColor="Gray" />
+        <PersonCard firstName="Susie" lastName="Geld" age={14} hairColor="Red" />
+        <PersonCard firstName="Percy" lastName="Jackson" age={18} hairColor="Blonde" />
+    </div>
+  );
+}
+
+export default App;
